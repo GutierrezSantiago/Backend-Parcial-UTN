905a1782e8331642247f5af54a12d02de296b0af
José Cabral <cabraljose1210@gmail.com>
2023-10-26 00:13:40 -0300

    Paquetes service.implementations, entity.response, entity.request

---
+++ northwind/src/main/java/ar/edu/frc/utn/bda3k4/northwind/entities/response/ResponseCustomer.java
@@ -0,0 +1,4 @@
+package ar.edu.frc.utn.bda3k4.northwind.entities.response;
+
+public class ResponseCustomer {
+}
---
+++ northwind/src/main/java/ar/edu/frc/utn/bda3k4/northwind/services/implementations/CustomerServiceImpl.java
@@ -0,0 +1,4 @@
+package ar.edu.frc.utn.bda3k4.northwind.services.implementations;
+
+public class CustomerServiceImpl {
+}
