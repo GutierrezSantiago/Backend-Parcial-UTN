bffff5f947700c739965eeffabd4be8c64ffcff8
José Cabral <cabraljose1210@gmail.com>
2023-10-26 00:05:54 -0300

    Repository

--- northwind/src/main/java/ar/edu/frc/utn/bda3k4/northwind/repositories/EmployeeRepository.java
+++ northwind/src/main/java/ar/edu/frc/utn/bda3k4/northwind/repositories/EmployeeRepository.java
@@ -1,9 +1,9 @@
 package ar.edu.frc.utn.bda3k4.northwind.repositories;
 
-import ar.edu.frc.utn.bda3k4.northwind.entities.Category;
+import ar.edu.frc.utn.bda3k4.northwind.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
--- northwind/src/main/java/ar/edu/frc/utn/bda3k4/northwind/repositories/ProductRepository.java
+++ northwind/src/main/java/ar/edu/frc/utn/bda3k4/northwind/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package ar.edu.frc.utn.bda3k4.northwind.repositories;
 
 import ar.edu.frc.utn.bda3k4.northwind.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
